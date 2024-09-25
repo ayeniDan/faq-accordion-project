f3de2ae5cda96d4fec053a7b3cccc4dd42de7f3e
Daniel Ayeni <dayeni2022@gmail.com>
2024-09-25 18:10:57 +0100

    finished faq project

---
+++ index.js
@@ -0,0 +1,32 @@
+const ul = document.querySelector('ul');
+const answers = ul.querySelectorAll('.answer');
+const minusIcons = ul.querySelectorAll('.iconMinus');
+
+answers.forEach((answer) => answer.classList.add('hidden'));
+minusIcons.forEach((answer) => answer.classList.add('hidden'));
+answers[0].classList.remove('hidden');
+minusIcons[0].classList.remove('hidden');
+ul.querySelector('.iconPlus').classList.add('hidden');
+
+function toggleAnswer(e) {
+	if (e.target.classList.contains('iconPlus')) {
+		e.target.classList.add('hidden');
+		e.target.nextElementSibling.classList.remove('hidden');
+		e.target.parentElement.parentElement.nextElementSibling.classList.remove(
+			'hidden'
+		);
+	} else if (e.target.classList.contains('iconMinus')) {
+		e.target.classList.add('hidden');
+		e.target.previousElementSibling.classList.remove('hidden');
+		e.target.parentElement.parentElement.nextElementSibling.classList.add(
+			'hidden'
+		);
+	}
+}
+
+function init() {
+	//event listeners
+	ul.addEventListener('click', toggleAnswer);
+}
+
+init();
